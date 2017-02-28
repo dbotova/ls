8f550bada750eb476acedaf824868596b59f2222
Dmitry <dz@SharkMacBookPro.local>
2017-02-27 22:32:07 -0800

    fix for lstdir

--- util/listdir.c
+++ util/listdir.c
@@ -22,26 +22,20 @@ void listdir(char *name, int level)
     if (!(entry = readdir(dir)))
         return;
 
-    do {
+    while ((entry = readdir(dir)) != NULL)
+    {
         if (entry->d_type == DT_DIR) 
         {
             char path[1024];
             int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
             path[len] = 0;
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-                continue;
-            printf("%*s[%s]\n", level*2, "", entry->d_name);
+            if (entry->d_name[0] == '.')
+                continue ;
+            ft_printf("%*s./%s\n", level*2, "", entry->d_name);
             listdir(path, level + 1);
         }
         else
-        {
-        	 printf("%*s- %s\n", level*2, "", entry->d_name);
-   //      	//while ((entry = readdir(dir)) != NULL)
-			// {
-			// 	if (entry->d_name[0] != '.')
-			// 		ft_printf("%s\n", entry->d_name);
-			// }
-        }
-    } while ((entry = readdir(dir)) != NULL);
+        	 ft_printf("%*s%s\n", level*2, "", entry->d_name);
+    }
     closedir(dir);
 }
